3cae9871c901ffed98e10378adc7b06dfd66a848
Iasonas Tsalemis <87096813+TsalemisIasonas@users.noreply.github.com>
2023-05-19 14:45:28 +0300

    Time: 48 ms (10.83%), Space: 27.5 MB (65.08%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        return binarySearch(nums,target,0,nums.size()-1);
+        int start = 0;
+        int end = nums.size() -1;
         
+        while (start<=end){
+            int mid = start + (end-start)/2;
+            if (nums[mid] == target){
+                return mid;
+            }else if (nums[mid] > target){
+                end = mid-1;
+            }else start = mid+1;
+        }
+        return -1;
     }
-    int binarySearch(vector<int>& nums, int target,int start, int end){
-        if (start>end){
-            return -1;
-        }
-        int mid = start + (end-start)/2;
-        if (nums[mid] == target){
-            return mid;
-        }
-        else if (nums[mid] < target){
-            return binarySearch(nums,target,mid+1,end);
-        }
-        else {
-            return binarySearch(nums,target,start,mid-1);
-        }
-    }
+
 };
